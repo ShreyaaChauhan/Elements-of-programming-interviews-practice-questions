d55d3baf21664854e3ad2f60c620944c89e3c7cb
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2022-09-22 14:52:38 +0200

    initial commit

---
+++ Primitive types/count_no_of_bits.py
@@ -0,0 +1,12 @@
+'''
+    complexity = o(n)
+'''
+def count_bits(x: int) -> int:
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x>>=1
+    return num_bits
+    
+no_of_bits = count_bits(10)
+print(no_of_bits)
