75cfbb5ae5d1b326d955156cc124e69f0fd8c02f
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2022-10-18 22:11:32 +0200

    18th October 2022

--- Binary search/binary_search.py
+++ Binary search/binary_search.py
@@ -1,4 +1,6 @@
-# Brute Force approach
+"""-----*BRUTE FORCE APPROACH*-----*TIME COMPLEXITY-O(n)*-----*SPACE COMPLEXITY - O(n)*-----"""
+
+
 def locate_card(cards, query):
     print(cards)
     pos = 0
@@ -11,18 +13,24 @@ def locate_card(cards, query):
             return False
 
 
-cards = [4, 2, 1, -1]
-query = 4
-tests = []
+"""-----*OPTIMISED APPROACH*-----*TIME COMPLEXITY- *-----*SPACE COMPLEXITY - *-----"""
 
+
+"""-----*CORNER TEST CASE*-----"""
+tests = []
 tests.append({"input": {"cards": [13, 11, 7, 4, 3, 1, 0], "query": 7}, "output": 3})
 tests.append({"input": {"cards": [13, 11, 7, 4, 3, 1, 0], "query": 1}, "output": 6})
 tests.append({"input": {"cards": [4, 2, 1, -1], "query": 4}, "output": 0})
 tests.append({"input": {"cards": [4, 2, 1, -127], "query": -127}, "output": 3})
 tests.append({"input": {"cards": [6], "query": 6}, "output": 0})
 
-print(tests)
+
+"""-----*INPUT*-----"""
+cards = [4, 2, 1, -1]
+
+"""-----*OUTPUT*-----"""
+query = 4
+
+"""-----*EXECUTION*-----"""
 result = locate_card(cards, query)
 print(result)
-
-# print("testing use case--------",locate_card(**test['input']) == test['output'])
