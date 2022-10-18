4cbde7bdf310e9e4d5da5d87a7abc83150d35cf7
ShreyaaChauhan <shreyachauhan1113@gmail.com>
2022-10-19 00:59:46 +0200

    19th Octiber 2022

--- Binary search/binary_search.py
+++ Binary search/binary_search.py
@@ -1,7 +1,7 @@
-"""-----*BRUTE FORCE APPROACH*-----*TIME COMPLEXITY-O(n)*-----*SPACE COMPLEXITY - O(n)*-----"""
+"""-----*BRUTE FORCE APPROACH*-----*TIME COMPLEXITY-O(n)*-----*SPACE COMPLEXITY - O(1)*-----"""
 
 
-def locate_card(cards, query):
+def locate_card_1(cards, query):
     print(cards)
     pos = 0
     for card in cards:
@@ -18,18 +18,20 @@ def locate_card(cards, query):
 
 """-----*CORNER TEST CASE*-----"""
 tests = []
-tests.append({"input": {"cards": [13, 11, 7, 4, 3, 1, 0], "query": 7}, "output": 3})
-tests.append({"input": {"cards": [13, 11, 7, 4, 3, 1, 0], "query": 1}, "output": 6})
+tests.append({"input": {"cards": [13, 11, 7, 4, 3, 1, 0], "query": 7}, "output": 2})
+tests.append({"input": {"cards": [13, 11, 7, 4, 3, 1, 0], "query": 1}, "output": 5})
 tests.append({"input": {"cards": [4, 2, 1, -1], "query": 4}, "output": 0})
 tests.append({"input": {"cards": [4, 2, 1, -127], "query": -127}, "output": 3})
 tests.append({"input": {"cards": [6], "query": 6}, "output": 0})
 
+for test in tests:
+    print(locate_card(**test["input"]) == test["output"])
 
 """-----*INPUT*-----"""
-cards = [4, 2, 1, -1]
+cards = [13, 11, 7, 4, 3, 1, 0]
 
 """-----*OUTPUT*-----"""
-query = 4
+query = 7
 
 """-----*EXECUTION*-----"""
 result = locate_card(cards, query)
